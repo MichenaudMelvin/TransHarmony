c202d373fef776b99d9f506db2b8dcfa510605b7
Melvil Rigot <melvil.rigot@gmail.com>
2022-02-22 14:12:28 +0100

    Post pros + fx

---
+++ Unity/SpaceConsonance/Assets/Scripts/Emisive_Sopt.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emisive_Sopt : MonoBehaviour
+{
+   public Material Spot_1;
+   public Material Spot_2;
+
+   private void update{
+
+   }
+
+
+}
